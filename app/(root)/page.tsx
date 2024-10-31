b70e9d45539154c2aa665445323e68e3e7a6ff99
Hugo-walando <hugowalando@gmail.com>
2024-10-31 15:42:52 +0100

    added a postCard component, formatdate function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import PostCard from '@/components/PostCard';
 import SearchForm from '../../components/SearchForm';
 
 export default async function Home({
@@ -9,6 +10,20 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1 },
+      _id: 1,
+      description: 'this is a description.',
+      image:
+        'https://tagageek.com/wp-content/uploads/2024/09/We-Robot-10-10-Tesla-Invite-au-Devoilement-de-sa-Robotaxi.webp',
+      category: 'Robots',
+      title: 'We Robots',
+    },
+  ];
+
   return (
     <>
       <section className='pink_container'>
@@ -18,6 +33,21 @@ export default async function Home({
           let others discover what drives your curiosity.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Showing results for "${query}"` : 'Showing all posts'}
+        </p>
+
+        <ul className='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+            posts.map((post: PostCardType, index: number) => (
+              <PostCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p>No posts found</p>
+          )}
+        </ul>
       </section>
     </>
   );

7e9bb090e313461dc19d2bf8e8a4a9e7b4ce09a1
Hugo-walando <hugowalando@gmail.com>
2024-10-31 14:58:34 +0100

    created a search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import SearchForm from '../../components/SearchForm';
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    query?: string;
+  }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
-      <h1 className='heading'>Home</h1>
+      <section className='pink_container'>
+        <h1 className='heading'>Post Whatever you want!</h1>
+        <p className='sub-heading !max-w-3xl'>
+          Share an idea or a topic that excites you <br />
+          let others discover what drives your curiosity.
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
